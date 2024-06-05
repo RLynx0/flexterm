bce55dc49e198ef84fe5ab157a398c52af0b720e
RLynx0 <luc.signin96@gmail.com>
2024-06-05 15:21:40 +0200

    Added type Minimal

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub use flexbox::{ContentSpacing, Flexbox, Orientation};
+pub use flexbox::{ContentAlign, Flexbox, Flow};
 pub use size::{Size, SizeComponent};
 
 mod flexbox;
@@ -71,5 +71,48 @@ impl Flex for VerticalBar {
             .map(|_| self.0.to_string())
             .collect::<Vec<_>>()
             .join("\n")
+    }
+}
+
+/// A Flex container that always renders it's content as small as possible.
+/// Useful for constraining a `Flexbox`
+///
+/// # Examples
+/// ```
+/// use flexterm::{Flex, Flexbox, Minimal};
+///
+/// let cont = "some_string";
+/// let cont_size = cont.size();
+///
+/// // `Flexbox` will always be of size `Stretch`,
+/// // even if its content has a fixed size.
+/// let fbox = Flexbox::default()
+///     .add_item(cont)
+///     .take();
+/// let fbox_size = fbox.size();
+///
+/// // `Minimal` will always be of size `Fixed`,
+/// // even if its content has a variable size.
+/// let min = Minimal(fbox);
+/// let min_size = min.size();
+///
+/// assert_ne!(fbox_size, cont_size);
+/// assert_ne!(fbox_size, min_size);
+/// assert_eq!(min_size, cont_size);
+/// ```
+pub struct Minimal<T>(pub T);
+impl<T> Flex for Minimal<T>
+where
+    T: Flex,
+{
+    fn size(&self) -> Size {
+        let size = self.0.size();
+        Size {
+            height: SizeComponent::Fixed(size.height.min()),
+            width: SizeComponent::Fixed(size.width.min()),
+        }
+    }
+    fn render(&self, _: usize, _: usize) -> String {
+        self.0.render_minimal()
     }
 }
